aa4cdbdcf081178d3ea1ac85b058b9d36446da29
I2etr0 <yyaroslav43@gmail.com>
2025-04-07 12:10:07 +0300

    Add bold text

--- install_gitlab-runner.sh
+++ install_gitlab-runner.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
 # Update gutlab-runner repo:
-echo "Update gutlab-runner repo"
+echo "${bold}Update gutlab-runner repo${normal}"
 sleep 2
 curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
 
 # Install runner
-echo "Install gitlab-runner"
+echo "${bold}Install gitlab-runner${normal}"
 sleep 2
 sudo apt install -y gitlab-runner
